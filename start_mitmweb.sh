4e8790e1b42e68ad46530f2fa63d61b70c9cff7b
Trevor Mack <drummer8001@gmail.com>
2020-06-23 16:40:40 -0400

    change to intercept traffic by default

--- start_mitmweb.sh
+++ start_mitmweb.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-mitmweb --scripts ~/picobrew_firmware_force.py --mode transparent --web-port 9090 --web-host 0.0.0.0 &>> /var/log/mitmweb.log
+mitmweb --scripts ~/picobrew_intercept.py --mode transparent --web-port 9090 --web-host 0.0.0.0 &>> /var/log/mitmweb.log
+#mitmweb --scripts ~/picobrew_firmware_force.py --mode transparent --web-port 9090 --web-host 0.0.0.0 &>> /var/log/mitmweb.log
